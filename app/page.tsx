beff1c64c250a96877d75f4e4e4de5b9549154f5
Envidaniel <envidaniel@gmail.com>
2026-01-08 05:43:39 +0100

    add dark or light mood

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import ThemeToggle from "@/components/theme-toggle";
 
 
 export default function Home() {
   return (
     <> 
+    <div className="min-h-screen bg-white text-black dark:bg-black dark:text-white flex items-center justify-center">
+      <ThemeToggle/>
+    </div>
+
       <h1>hello my bro !</h1>
     </>
   )
