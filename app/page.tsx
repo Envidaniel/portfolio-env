8c429267dfceb77f4738d91e9141f6982200ee1d
Envidaniel <envidaniel@gmail.com>
2026-01-12 19:43:23 +0100

    new file

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,8 @@ import ThemeToggle from "@/components/theme-toggle";
 
 export default function Home() {
   return (
-    <> 
-    <div className="min-h-screen bg-white text-black dark:bg-black dark:text-white flex items-center justify-center">
-      <ThemeToggle/>
-    </div>
+    <>
 
-      <h1>hello my bro !</h1>
     </>
   )
 }
--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -5,18 +5,18 @@ import { useEffect, useState } from "react"
 
 
 export default function ThemeToggle() {
-    const {theme, setTheme} = useTheme()
+    const { theme, setTheme } = useTheme()
     const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
         setMounted(true)
-    },[])
+    }, [])
 
-    if(!mounted){
+    if (!mounted) {
         return null
     }
 
-    return(
+    return (
         <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
             className=" p-2 rounded-full border border-gray-300 dark:border-gray-700 hover:bg-gray-100 dark:hover:bg-gray-800 transition ">
             {theme === 'dark' ? '☀️' : '🌙'}
